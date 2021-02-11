b924a9e3ea8a9993577e957256fe285ef60763cc
mikekben <benmikek@gmail.com>
2021-02-10 22:01:08 -0500

    Added page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,29 @@
 require "test_helper"
 
+
+
+
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "My Testing Project"
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | My Testing Project"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get info" do
     get static_pages_info_url
     assert_response :success
-    assert_select "title", "Info | My Testing Project"
+    assert_select "title", "Info | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | My Testing Project"
+    assert_select "title", "About | #{@base_title}"
   end
 end
