52a2b236989801a4af6be36946d779bfaf14757a
mikekben <benmikek@gmail.com>
2021-02-10 21:51:51 -0500

    Added tests

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def info
   end
+
+  def about
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,10 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | My Testing Project"
   end
 
   test "should get info" do
     get static_pages_info_url
     assert_response :success
+    assert_select "title", "Info | My Testing Project"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | My Testing Project"
   end
 end
