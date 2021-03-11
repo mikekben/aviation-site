607a883b617770744e9b2f2d35518704b979e98b
mikekben <benmikek@gmail.com>
2021-03-11 10:40:17 -0500

    Added uniqueness validation for airports

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -4,10 +4,10 @@
 require 'coordinate'
 
 class Airport < ApplicationRecord
-    validates :iata, format: {with: /[A-Z]{3}/}
-    validates :icao, format: {with: /[A-Z][A-Z0-9]{3}/}
+    validates :iata, uniqueness: true, format: {with: /[A-Z]{3}/}
+    validates :icao, uniqueness: true, format: {with: /[A-Z][A-Z0-9]{3}/}
     validates :region, presence: true, format: {with: /([A-Z][A-Z]-\w{1,3})|([A-Z][A-Z])/}
-    validates :airport_name, presence: true
+    validates :airport_name, presence: true, uniqueness: true
     validates :city, presence: true
     validates :lat, presence: true, numericality: true
     validates :lon, presence: true, numericality: true
